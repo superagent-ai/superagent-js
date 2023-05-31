e6796561ad7f47c05492d61f0ddb89a5c22e8120
Ismail Pelaseyed <homanp@gmail.com>
2023-05-31 13:43:50 +0200

    Add `Typescript` support

---
+++ src/superagent.d.ts
@@ -0,0 +1,53 @@
+declare module 'superagentSDK' {
+  type Method = "GET" | "POST" | "DELETE";
+
+  interface Prompt {
+    name?: string | null;
+    input_variables?: string[];
+    template?: string | null;
+  }
+
+  interface Document {
+    name: string;
+    url: string;
+    type: string;
+    authorization?: string | null;
+  }
+
+  interface Agent {
+    name: string;
+    llm: string;
+    type?: string;
+    has_memory: boolean;
+  }
+
+  interface Prediction {
+    id: string;
+    input: string;
+    has_streaming: boolean;
+  }
+
+  export default class SuperagentSDK {
+    constructor(authToken: string);
+    private _request(method: Method, endpoint: string, data?: any): Promise<any>;
+    prompts(): {
+      delete(id: string): Promise<any>;
+      get(id: string): Promise<any>;
+      list(): Promise<any>;
+      create(prompt: Prompt): Promise<any>;
+    };
+    documents(): {
+      delete(id: string): Promise<any>;
+      list(): Promise<any>;
+      get(id: string): Promise<any>;
+      create(document: Document): Promise<any>;
+    };
+    agents(): {
+      delete(id: string): Promise<any>;
+      list(): Promise<any>;
+      get(id: string): Promise<any>;
+      create(agent: Agent): Promise<any>;
+      predict(prediction: Prediction): Promise<any>;
+    };
+  }
+}
