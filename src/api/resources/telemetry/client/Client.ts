a18399ea60a8e62a9d27cffd26bb1e02edb3cf84
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-02-12 22:31:52 +0000

    Release v0.2.5

--- src/api/resources/telemetry/client/Client.ts
+++ src/api/resources/telemetry/client/Client.ts
@@ -67,7 +67,7 @@ export class Telemetry {
                 Authorization: await this._getAuthorizationHeader(),
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "superagentai-js",
-                "X-Fern-SDK-Version": "v0.2.4",
+                "X-Fern-SDK-Version": "v0.2.5",
             },
             contentType: "application/json",
             queryParameters: _queryParams,
